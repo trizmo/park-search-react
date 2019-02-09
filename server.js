2f8e8776d2e9bf1c213acd40b5727b01d16dd192
Tristan Perera <tristan.e.perera@gmail.com>
2019-02-09 08:39:47 -0800

    prod changes

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(routes);
 // IF in Production:
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static("client/build"))
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"))
   } )
 }
 
